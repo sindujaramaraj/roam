2b99a1cd22149e96fccaee93c83d43aeeaf997ec
sinduja <sinduja@sinduja.(none)>
2014-03-03 19:30:07 +0530

    temp remove controllers

--- web.js
+++ web.js
@@ -21,13 +21,13 @@ app.use(app.router);
 app.use(express.static(__dirname + '/static'));
 
 //dynamically include routes (Controller)
-fs.readdirSync('./controller').forEach(function (file) {
+/*fs.readdirSync('./controller').forEach(function (file) {
   if(file.substr(-3) == '.js') {
       route = require('./controller/' + file);
       route.controller(app);
   }
 });
-
+*/
 //create server
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
