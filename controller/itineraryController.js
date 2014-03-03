69269f2c22302da8d5003685744ebdd58e678b75
sinduja <sinduja@sinduja.(none)>
2014-03-03 18:55:36 +0530

    added routes in itinerary controller

--- controller/itineraryController.js
+++ controller/itineraryController.js
@@ -1,9 +1,14 @@
 
-function ItineraryController() {
+module.exports.controller = function(app) {
+	app.get('/createItinerary', function(req, res) {
+		
+	});
+	
+	app.get('/getItinerary', function(req, res) {
+		
+	});
+	
+	app.get('/updateItinerary', function(req, res) {
+		
+	});
 }
-
-ItineraryController.prototype = {
-   controller: function() {}   
-}
-
-module.exports = new ItineraryController();
